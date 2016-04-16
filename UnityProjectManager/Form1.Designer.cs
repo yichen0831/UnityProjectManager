641b0413708c8b24086af7bebd6629049ee4c9f4
Yichen Huang (Eugene) <yichen0831@gmail.com>
2016-04-16 23:33:36 +0800

    Init commit

---
+++ UnityProjectManager/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace UnityProjectManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 17;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(632, 259);
+            this.listBox1.TabIndex = 0;
+            // 
+            // removeButton
+            // 
+            this.removeButton.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.removeButton.Location = new System.Drawing.Point(568, 280);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(75, 23);
+            this.removeButton.TabIndex = 1;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 315);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "UnityProjectManager";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button removeButton;
+    }
+}
+
---
+++ UnityProjectManager/Form1.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnityProjectManager
+{
+    public partial class Form1 : Form
+    {
+        const string registryRoot = "Software\\Unity Technologies\\Unity Editor 5.x";
+        List<string> recentProjectPaths = new List<string>(); // store recent project paths in the registry
+        Dictionary<string, string> subkeyDict = new Dictionary<string, string>(); // map readablePath to subkey
+
+        public Form1()
+        {
+            InitializeComponent();
+            removeButton.Enabled = false;
+            LoadUnityProjectList();
+        }
+
+        private void LoadUnityProjectList()
+        {
+            recentProjectPaths.Clear();
+            subkeyDict.Clear();
+
+            RegistryKey keyUnity = Registry.CurrentUser.OpenSubKey(registryRoot, false);
+            string[] subkeys = keyUnity.GetValueNames();
+
+            foreach (var subkey in subkeys)
+            {
+                if (subkey.StartsWith("RecentlyUsedProjectPaths"))
+                {
+                    recentProjectPaths.Add(subkey);
+                }
+            }
+
+            byte[] path;
+            foreach (var subkey in recentProjectPaths)
+            {
+                path = (byte[])Registry.GetValue("HKEY_CURRENT_USER\\Software\\Unity Technologies\\Unity Editor 5.x", subkey, 0);
+                string readablePath = Encoding.Default.GetString(path);
+                subkeyDict.Add(readablePath, subkey);
+                listBox1.Items.Add(readablePath);
+            }
+
+            keyUnity.Close();
+
+            if (listBox1.Items.Count > 0)
+            {
+                removeButton.Enabled = true;
+            }
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count == 0)
+            {
+                removeButton.Enabled = false;
+                return;
+            }
+
+            if (listBox1.SelectedIndex != -1)
+            {
+                RegistryKey keyUnity = Registry.CurrentUser.OpenSubKey(registryRoot, true);
+                string selected = (string)listBox1.SelectedItem;
+                string subkey = subkeyDict[selected];
+                keyUnity.DeleteValue(subkey);
+
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+                subkeyDict.Remove(selected);
+
+                keyUnity.Close();
+            }
+
+            if (listBox1.Items.Count == 0)
+            {
+                removeButton.Enabled = false;
+            }
+        }
+    }
+}
